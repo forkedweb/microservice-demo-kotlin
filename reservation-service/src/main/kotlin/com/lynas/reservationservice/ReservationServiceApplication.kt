d0085dd11f4b6bd7d45fc9766f0a37b39f9cdf50
LynAs <szlynas@gmail.com>
2017-10-14 21:18:35 +0600

    reservation service discoverable by service discovery server

--- reservation-service/src/main/kotlin/com/lynas/reservationservice/ReservationServiceApplication.kt
+++ reservation-service/src/main/kotlin/com/lynas/reservationservice/ReservationServiceApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.JpaRepository
@@ -14,6 +15,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class ReservationServiceApplication{
 
