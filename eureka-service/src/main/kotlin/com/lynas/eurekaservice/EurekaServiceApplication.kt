cb8fed8182cb71215cf9e79f0aff2a4746b66f7b
LynAs <szlynas@gmail.com>
2017-10-14 21:12:38 +0600

    discovery server  added

---
+++ eureka-service/src/main/kotlin/com/lynas/eurekaservice/EurekaServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.lynas.eurekaservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class EurekaServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EurekaServiceApplication::class.java, *args)
+}
